631eed71116a0cd65fa5b686e0075f743d809f28
Loki_79 <76516343+loki79uk@users.noreply.github.com>
2024-12-30 10:22:01 +0000

    Fix for #196
    - Should fix uneven loading of mixed length logs
    - Now stores previous loaded length in savegame file

--- UniversalAutoload.lua
+++ UniversalAutoload.lua
@@ -1634,6 +1634,7 @@ function UniversalAutoload:onPostLoad(savegame)
 			spec.currentLayerCount = 0
 			spec.currentLayerHeight = 0
 			spec.nextLayerHeight = 0
+			spec.lastAddedLoadLength = 0
 			spec.currentLoadAreaIndex = 1
 			spec.resetLoadingLayer = false
 			spec.resetLoadingPattern = false
@@ -1658,6 +1659,7 @@ function UniversalAutoload:onPostLoad(savegame)
 			spec.currentLayerCount = savegame.xmlFile:getValue(key.."#layerCount", 0)
 			spec.currentLayerHeight = savegame.xmlFile:getValue(key.."#layerHeight", 0)
 			spec.nextLayerHeight = savegame.xmlFile:getValue(key.."#nextLayerHeight", 0)
+			spec.lastAddedLoadLength = savegame.xmlFile:getValue(key.."#lastLoadLength", 0)
 			spec.currentLoadAreaIndex = savegame.xmlFile:getValue(key.."#loadAreaIndex", 1)
 			spec.resetLoadingLayer = false
 			spec.resetLoadingPattern = false
@@ -1732,6 +1734,7 @@ function UniversalAutoload:saveToXMLFile(xmlFile, key, usedModNames)
 	xmlFile:setValue(saveKey.."#layerCount", spec.currentLayerCount or 0)
 	xmlFile:setValue(saveKey.."#layerHeight", spec.currentLayerHeight or 0)
 	xmlFile:setValue(saveKey.."#nextLayerHeight", spec.nextLayerHeight or 0)
+	xmlFile:setValue(saveKey.."#lastLoadLength", spec.lastAddedLoadLength or 0)
 	xmlFile:setValue(saveKey.."#loadAreaIndex", spec.currentLoadAreaIndex or 1)
 	
 end
@@ -3438,12 +3441,15 @@ function UniversalAutoload:createLoadingPlace(containerType)
 	else
 		spec.currentLoadWidth = spec.currentLoadWidth + addedLoadWidth
 		
-		if spec.lastAddedLoadLength and spec.lastAddedLoadLength < addedLoadLength and not containerType.isSplitShape then
-			print("EXCEEDED LAST ADDED LOAD LENGTH")
-			-- local difference = addedLoadLength - spec.lastAddedLoadLength
-			-- spec.currentLoadLength = spec.currentLoadLength + difference
-			-- spec.lastAddedLoadLength = addedLoadLength
-			return
+		if spec.lastAddedLoadLength and spec.lastAddedLoadLength < addedLoadLength then
+			if containerType.isSplitShape then
+				local difference = addedLoadLength - spec.lastAddedLoadLength
+				spec.currentLoadLength = spec.currentLoadLength + difference
+				spec.lastAddedLoadLength = addedLoadLength
+			else
+				print("EXCEEDED LAST ADDED LOAD LENGTH")
+				return
+			end
 		end
 	end
 
@@ -3573,6 +3579,7 @@ function UniversalAutoload:resetLoadingArea()
 	UniversalAutoload.resetLoadingPattern(self)
 	spec.trailerIsFull = false
 	-- spec.partiallyUnloaded = false
+	spec.lastAddedLoadLength = 0
 	spec.currentLoadAreaIndex = 1
 	spec.lastLoadAttempt = nil
 	spec.loadingAreaIsFull = nil
